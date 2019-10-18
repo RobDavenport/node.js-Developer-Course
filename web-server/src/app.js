e0a90b44696ec4813999cc0ac210ada25e81178c
Zerve0 <zerve0@hotmail.com>
2019-10-18 18:45:49 +0900

    s7 complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,6 +48,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found.',
+        name: 'Robbie Davenport'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'Robbie Davenport'
+    })
+})
+
 app.listen(port, () => {
     console.log('Server is up on ' + port);
 })
